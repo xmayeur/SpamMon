46881633e30531f1a5991b93d8c662cdb588d229
xmayeur <Pignouf2723>
2016-11-13 17:52:09 +0100

    XMA 20161113

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     author='X. Mayeur',
     author_email='xavier@mayeur.be',
     description='eMail Spam monitoring using IMAP',
-    requires=['eventlet', 'cryptography', 'ConfigParser']
+    requires=['eventlet', 'cryptography', 'ConfigParser', 'Key']
 )
