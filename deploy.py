ce56f7c46da91ab0a497dacbafcf6bc66da5ede0
xmayeur <Pignouf2723>
2016-11-20 22:25:05 +0100

    Add deploy.py script to copy updates to the target server and (re)start the service

---
+++ deploy.py
@@ -0,0 +1,25 @@
+from fabric.api import *
+
+# from py_compile import compile
+
+env.host_string = 'rpiMON'
+env.user = 'pi'
+env.use_ssh_config = True
+
+# Deploy files
+deploy_list = ['spam.py', 'SpamMon.py']
+script = 'SpamMon'
+
+with cd('~/SpamMon'):
+    for f in deploy_list:
+        put(f)
+
+# Stop service, update it and re-start
+with settings(warn_only=True):
+    run('sudo service ' + script + ' stop')
+
+with cd('/etc/init.d'):
+    put(local_path=script + '.', remote_path='/etc/init.d/' + script, use_sudo=True, mode=0755)
+    sudo('sudo update-rc.d ' + script + ' defaults')
+
+sudo('sudo service ' + script + ' start')
