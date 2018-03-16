90693a7972587dc1941a8b3df5ad1cc45179b908
Xavier Mayeur <xavier@mayeur.be>
2018-03-16 22:53:51 +0100

    Migrated to Python 3  - make use of vault microservice to retrieve username & password

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 git pull
+docker rm -f spammon
 docker build -t spammon . && \
 docker tag spammon xmayeur\spammon && \
-docker push xmayeur/spammon
+docker push xmayeur/spammon && \
+exec ./spammon.sh
+
+
