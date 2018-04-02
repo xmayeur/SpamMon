182b2d7585aa11a6ba5d1b6e823aa536e1c16ee6
Xavier Mayeur <xavier@mayeur.be>
2018-04-02 12:52:27 -0700

    travis

--- spammon.sh
+++ spammon.sh
@@ -2,5 +2,7 @@
 
 # wget  https://raw.githubusercontent.com/xmayeur/spammon/master/SpamMon.conf
 
-docker run -ti --name spammon --dns 8.8.8.8 -v /root/:/conf/ -v /var/log:/var/log/ spammon
+docker rm -f spammon
+docker pull xmayeur/spammon
+docker run -ti --name spammon -v /root/:/conf/ -v /var/log:/var/log/ xmayeur/spammon
 
